aacc51ccbe56876e5b96ad92f8f10b8c49137c0a
ssortia <alexkiyan.lug@gmail.com>
2022-11-09 10:41:53 +0300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ajvinstance
+
+### Установка 
+```
+npm i @ilb/ajvinstance
+```
+
+### Использование
+```js
+import { ajv } from '@ilb/ajvinstance';
+
+const validator = ajv.compile(schema);
+```
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import Ajv from 'ajv';
+import JSONSchemaBridge from 'uniforms-bridge-json-schema';
+import localize from 'ajv-i18n';
+import registerAjvKeywords from './registerAjvKeywords';
+
+export const ajv = new Ajv({ allErrors: true, useDefaults: true, coerceTypes: true, strict: false });
+ajv.addKeyword('uniforms');
+ajv.addKeyword('options');
+
+// email or empty string
+ajv.addFormat(
+  'email',
+  /(^[a-z\d.!#$%&'*+/=?^_`{|}~-]+@[a-z\d](?:[a-z\d-]{0,61}[a-z\d])?(?:\.[a-z\d](?:[a-z\d-]{0,61}[a-z\d])?)*$)|(^$)/
+);
+
+registerAjvKeywords(ajv);
+
+function createValidator(schema, additionalValidator) {
+  const validator = ajv.compile(schema);
+
+  return (model) => {
+    let errors = [];
+
+    validator(model);
+
+    if (validator.errors && validator.errors.length) {
+      errors = validator.errors;
+    }
+
+    if (additionalValidator) {
+      errors = errors.concat(additionalValidator(model));
+    }
+
+    if (errors.length) {
+      localize.ru(errors);
+      return { details: errors };
+    }
+  };
+}
+
+export function createSchemaBridge(schema, additionalValidator) {
+  return new JSONSchemaBridge(schema, createValidator(schema, additionalValidator));
+}
