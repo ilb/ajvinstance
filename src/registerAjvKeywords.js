39e6c5197e2123179e053befdfb088254aff3d2b
maksim <mkozhevnikov23@gmail.com>
2022-12-21 21:21:59 +0400

    add countWords

--- src/registerAjvKeywords.js
+++ src/registerAjvKeywords.js
@@ -100,5 +100,21 @@ export default (ajv) => {
     },
   });
 
+  ajv.addKeyword('countWords', {
+    type: 'string',
+    errors: true,
+    validate: function validate(schema, data, parent, key) {
+      validate.errors = [
+        {
+          keyword: 'countWords',
+          message: 'Должно быть минимум ' + schema + 'слов',
+          params: { keyword: 'countWords' },
+        },
+      ];
+      // if()
+      return !(data.trim().split(' ').length < schema);
+    },
+  });
+
   return ajv;
 };
